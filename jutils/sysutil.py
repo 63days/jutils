0959b56cc951813c26e381849a7515bd6e2f74d9
Juil Koo <63days@kaist.ac.kr>
2023-10-16 10:56:56 +0900

    add save_config save_commandi n sysutil

--- jutils/sysutil.py
+++ jutils/sysutil.py
@@ -1,17 +1,22 @@
-import rich.syntax
-from datetime import datetime
-import pytorch_lightning as pl
 import argparse
-import PIL
 import gc
-import torch
+from datetime import datetime
+from pathlib import Path
+from typing import List, Union
+
+import PIL
+import pytorch_lightning as pl
+import rich.syntax
 import rich.tree
-from omegaconf import DictConfig, OmegaConf
+import torch
 import wandb
+import yaml
+from omegaconf import DictConfig, OmegaConf
+
 
 def print_config(
     config,
-    fields = (
+    fields=(
         "trainer",
         "model",
         "datamodule",
@@ -49,19 +54,22 @@ def print_config(
     with open("config_tree.log", "w") as fp:
         rich.print(tree, file=fp)
 
+
 def clean_gpu():
     gc.collect()
     torch.cuda.empty_cache()
 
+
 def str2bool(v):
     if isinstance(v, bool):
-       return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 
 def wandb_log(wandb_on: bool, dic: dict):
     if not wandb_on:
@@ -69,9 +77,10 @@ def wandb_log(wandb_on: bool, dic: dict):
 
     wandb.log(dic)
 
+
 def wandb_log_image(wandb_on: bool, log_name: str, images: dict):
     if not wandb_on:
-        return 
+        return
 
     if isinstance(images, PIL.Image.Image):
         images = [wandb.Image(images)]
@@ -83,12 +92,51 @@ def wandb_log_image(wandb_on: bool, log_name: str, images: dict):
 
     wandb_log(wandb_on, {log_name: images})
 
+
 def get_wandb_logger(pl_module):
     for logger in pl_module.logger:
         if isinstance(logger, pl.loggers.wandb.WandbLogger):
             return logger
     return None
 
+
 def get_current_time():
     now = datetime.now().strftime("%m-%d-%H%M%S")
     return now
+
+
+def save_command(path: Union[str, Path], sysargv: List):
+    if isinstance(path, str):
+        path = Path(path)
+    if path.suffix == "":
+        path.mkdir(exist_ok=True, parents=True)
+        path = path / "cmd.txt"
+    elif path.suffix == ".txt":
+        dirpath = path.parent
+        dirpath.mkdir(exist_ok=True, parents=True)
+    else:
+        raise ValueError
+
+    with open(path, "w") as f:
+        cmd = ""
+        for arv in sysargv:
+            cmd += f"{arv} "
+        f.write(f"{cmd}")
+
+    print(f"[*] Saved command at {path}")
+
+
+def save_config(path: Union[str, Path], dic):
+    if isinstance(path, str):
+        path = Path(path)
+    if path.suffix == "":
+        path.mkdir(exist_ok=True, parents=True)
+        path = path / "config.yaml"
+    elif path.suffix == ".yaml" or path.suffix == ".yml":
+        dirpath = path.parent
+        dirpath.mkdir(exist_ok=True, parents=True)
+    else:
+        raise ValueError
+
+    with open(path, "w") as f:
+        yaml.dump(dic, f)
